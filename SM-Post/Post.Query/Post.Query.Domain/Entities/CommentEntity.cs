12f035d0f47ccf6bcf128d210fe4bf3bbc68b0ca
Guilherme Santos <gsantos@br-itsoftware.com.br>
2023-06-14 08:44:15 -0300

    feat: Add Domain Layer by Section 9

---
+++ SM-Post/Post.Query/Post.Query.Domain/Entities/CommentEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Post.Query.Domain.Entities
+{
+    [Table("Comment")]
+    public class CommentEntity
+    {
+        [Key]
+        public Guid CommentId { get; set; }
+        public string? Username { get; set; }
+        public string? Comment { get; set; }
+        public DateTime CommentDate { get; set; }
+        public DateTime? Edited { get; set; }
+        public Guid PostId { get; set; }
+
+        [System.Text.Json.Serialization.JsonIgnore]
+        public PostEntity? Post { get; set; }
+    }
+}
---
+++ SM-Post/Post.Query/Post.Query.Domain/Repositories/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using Post.Query.Domain.Entities;
+
+namespace Post.Query.Domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task CreateAsync(PostEntity post);
+        Task UpdateAsync(PostEntity post);
+        Task DeleteAsync(Guid id);
+        Task<PostEntity?> GetByIdAsync(Guid id);
+        Task<List<PostEntity>> ListAllAsync();
+        Task<List<PostEntity>> ListByAuthorAsync(string author);
+        Task<List<PostEntity>> ListWithLikesAsync(int numberOfLikes);
+        Task<List<PostEntity>> ListWithCommentsAsync();
+    }
+}
